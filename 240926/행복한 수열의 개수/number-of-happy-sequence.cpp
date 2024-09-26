bbbe95f3b20718bc8f09b4aca67f179cd2a1ece6
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2024-09-26 12:40:09 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240926/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240926/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -10,9 +10,10 @@ int main() {
 
     vector<vector<int>> arr(n, vector<int>(n));
 
-    // 입력 받기
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
             cin >> arr[i][j];
         }
     }
@@ -20,55 +21,46 @@ int main() {
     int ans = 0;
 
     // 가로 검사
-    for(int i=0; i<n; i++) {
-        int cnt_row = 1;  // 연속된 원소 개수 카운트
-        bool found_happy_row = false;  // 행복한 수열을 찾았는지 여부
+    for(int i=0; i<n; i++)
+    {
+        int cnt_row = 1;
+        int cnt_col = 1;
+        bool s_row = false;
+        bool s_col = false;
 
-        for(int j=1; j<n; j++) {
-            if(arr[i][j] == arr[i][j-1]) {
+        for(int j=1; j<n; j++)
+        {
+            if(arr[i][j] == arr[i][j-1])
+            {
                 cnt_row++;
             } else {
-                cnt_row = 1;  // 연속되지 않으면 다시 1로 초기화
+                if(cnt_row >= m) {
+                    ans++;
+                    s_row = true;
+                }
+                cnt_row = 1;
             }
 
-            // 연속된 원소가 m개 이상인 경우
-            if(cnt_row >= m) {
-                found_happy_row = true;
-            }
-        }
-
-        // 해당 행이 행복한 수열이면 카운트
-        if(found_happy_row) {
-            ans++;
-        }
-    }
-
-    // 세로 검사
-    for(int i=0; i<n; i++) {
-        int cnt_col = 1;  // 연속된 원소 개수 카운트
-        bool found_happy_col = false;  // 행복한 수열을 찾았는지 여부
-
-        for(int j=1; j<n; j++) {
-            if(arr[j][i] == arr[j-1][i]) {
+            if(arr[j][i] == arr[j-1][i])
+            {
                 cnt_col++;
             } else {
-                cnt_col = 1;  // 연속되지 않으면 다시 1로 초기화
-            }
-
-            // 연속된 원소가 m개 이상인 경우
-            if(cnt_col >= m) {
-                found_happy_col = true;
+                if(cnt_row >= m) {
+                    ans++;
+                    s_col = true;
+                }
+                cnt_col = 1;
             }
         }
 
-        // 해당 열이 행복한 수열이면 카운트
-        if(found_happy_col) {
-            ans++;
-        }
+        if(!s_row && cnt_row >= m) ans++;
+        if(!s_col && cnt_col >= m) ans++;
+
+
     }
 
-    // 행복한 수열의 개수 출력
-    cout << ans << endl;
+
+    cout << (ans);
 
     return 0;
 }
