d6b394baf60e8aefa832399bab72fdd309409211
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2024-09-26 12:42:26 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240926/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240926/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -20,44 +20,54 @@ int main() {
 
     int ans = 0;
 
-    // 가로 검사
-    for(int i=0; i<n; i++)
-    {
-        int cnt_row = 1;
-        int cnt_col = 1;
-        bool s_row = false;
-        bool s_col = false;
-
-        for(int j=1; j<n; j++)
-        {
-            if(arr[i][j] == arr[i][j-1])
-            {
-                cnt_row++;
-            } else {
-                if(cnt_row >= m) {
-                    ans++;
-                    s_row = true;
-                }
-                cnt_row = 1;
-            }
-
-            if(arr[j][i] == arr[j-1][i])
-            {
-                cnt_col++;
-            } else {
-                if(cnt_row >= m) {
-                    ans++;
-                    s_col = true;
-                }
-                cnt_col = 1;
-            }
-        }
-
-        if(!s_row && cnt_row >= m) ans++;
-        if(!s_col && cnt_col >= m) ans++;
-
-
+    // 검사
+    if(n==1) {
+        if(m == 1) ans = 1;
     }
+    else
+    {
+
+        for(int i=0; i<n; i++)
+        {
+            int cnt_row = 1;
+            int cnt_col = 1;
+            bool s_row = false;
+            bool s_col = false;
+
+            
+
+            for(int j=1; j<n; j++)
+            {
+                if(arr[i][j] == arr[i][j-1])
+                {
+                    cnt_row++;
+                } else {
+                    if(cnt_row >= m) {
+                        ans++;
+                        s_row = true;
+                    }
+                    cnt_row = 1;
+                }
+
+                if(arr[j][i] == arr[j-1][i])
+                {
+                    cnt_col++;
+                } else {
+                    if(cnt_row >= m) {
+                        ans++;
+                        s_col = true;
+                    }
+                    cnt_col = 1;
+                }
+            }
+
+            if(!s_row && cnt_row >= m) ans++;
+            if(!s_col && cnt_col >= m) ans++;
+
+
+        }
+    }
+
 
 
     cout << (ans);
