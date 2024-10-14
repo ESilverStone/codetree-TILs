87bb4c0cf707274367d678ae12e70035415f00f3
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2024-10-14 14:19:32 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241014/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int MAX_NUM = 1001;
+vector<int> memo;
+
+int fibo(int n) {
+    if(memo[n] != -1)
+    {
+        return memo[n];
+    }
+
+    if(n > 3)
+    {
+        memo[n] = fibo(n-2) + fibo(n-3);
+    }
+
+    return memo[n];
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    memo.resize(MAX_NUM, -1);
+
+    memo[0] = 0;
+    memo[1] = 0;
+    memo[2] = 1;
+    memo[3] = 1;
+
+    fibo(n);
+
+    if(memo[n] == -1 || 0)
+    {
+        cout << 0;
+    } else {
+        cout << memo[n] % 10007;
+    }
+
+    return 0;
+}
